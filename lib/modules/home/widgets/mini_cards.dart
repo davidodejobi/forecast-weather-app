0b4b3c067ddf9fd4d7e4182044a4bdf86016dd89
David Odejobi <talk2engineerdavid@gmail.com>
2022-07-06 15:08:25 +0100

    added scrolling animation

--- lib/modules/home/widgets/mini_cards.dart
+++ lib/modules/home/widgets/mini_cards.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
-class MiniCards extends StatelessWidget {
+class MiniCards extends StatefulWidget {
   const MiniCards({
     required this.time,
     required this.conditionIcon,
@@ -14,10 +14,43 @@ class MiniCards extends StatelessWidget {
   final List temperature;
 
   @override
+  State<MiniCards> createState() => _MiniCardsState();
+}
+
+class _MiniCardsState extends State<MiniCards> {
+  final ScrollController _controller = ScrollController();
+
+// This is what you're looking for!
+  void _scrollDown() {
+    _controller
+        .animateTo(
+      _controller.position.maxScrollExtent,
+      duration: const Duration(seconds: 4),
+      curve: Curves.slowMiddle,
+    )
+        .then((value) {
+      _controller.animateTo(
+        _controller.position.minScrollExtent,
+        duration: const Duration(seconds: 4),
+        curve: Curves.slowMiddle,
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 1), () {
+      _scrollDown();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
+        controller: _controller,
         scrollDirection: Axis.horizontal,
-        itemCount: time.length,
+        itemCount: widget.time.length,
         itemBuilder: (context, index) {
           return Container(
             padding: const EdgeInsets.symmetric(
@@ -35,18 +68,18 @@ class MiniCards extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  time[index],
+                  widget.time[index],
                   style: Theme.of(context).textTheme.subtitle1,
                 ),
                 const SizedBox(height: 8.0),
                 WebsafeSvg.asset(
-                  'assets/svg/${conditionIcon[index]}.svg',
+                  'assets/svg/${widget.conditionIcon[index]}.svg',
                   width: 32.0,
                   height: 32.0,
                 ),
                 const SizedBox(height: 8.0),
                 Text(
-                  '${temperature[index].round()}°',
+                  '${widget.temperature[index].round()}°',
                   style: Theme.of(context).textTheme.subtitle1,
                 ),
               ],
